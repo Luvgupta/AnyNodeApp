a8428bcef7baf738ada3cdf54ae540817ed6a042
Luv Gupta <luv@capiot.com>
2015-11-27 16:29:54 +0530

    Asd

--- AnyNodeApp/web_server/iotEmulator/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl http://localhost/b/r
-curl http://localhost/b/i
-sleep 1
-curl http://localhost/b/p
--- AnyNodeApp/web_server/public/templates/dash/dashCtrl.js
+++
@@ -1,3 +0,0 @@
-app.controller('DashCtrl', function ($state, $scope) {
-
-});
